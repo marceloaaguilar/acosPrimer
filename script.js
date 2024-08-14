7c3e3e72000a4f56dab4c3256115b6bd1b82dd12
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2024-08-14 01:20:51 -0300

    Novas implementações

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
+function processaForm(e){
+    e.preventDefault();
+    const nomeOrcamento     = document.getElementById("nameClient");
+    const telefoneOrcamento = document.getElementById("phoneNumber");
+    const emailOrcamento    = document.getElementById("email");
+    const produtoOrcamento  = document.getElementById("typeProduct");
+
+    fetch("classes/enviaEmail.php", {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "nome"      : nomeOrcamento.value,
+            "telefone"  : telefoneOrcamento.value,
+            "email"     : emailOrcamento.value,
+            "produto"   : produtoOrcamento.value
+        })
+    }).then(function (response) {
+        if (response.status == 200){
+            window.location.href = 'https://www.acosprimer.com.br/obrigado';
+        }
+    })
+}
+
+const formOrcamento = document.getElementById("formOrcamentoPrimer");
+formOrcamento.addEventListener("submit", processaForm);
+
+
+
 window.addEventListener("load", function (event) {
+
+    if (window.location.pathname == '/obrigado'){
+        setTimeout(()=> {
+            window.location. href = 'https://api.whatsapp.com/send/?phone=5521959142173&text=Ol%C3%A1%2C+gostaria+de+fazer+uma+cota%C3%A7%C3%A3o%21+&type=phone_number&app_absent=0'; 
+        },1000)
+    }
+
+
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 1,
         spaceBetween: 40,
@@ -21,4 +59,4 @@ window.addEventListener("load", function (event) {
             }
         }
     });
-  });
+});
